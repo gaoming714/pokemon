40060cb6b9fabc2a24b5b815a651192a53f9c4e7
gaoming714 <gaoming714@outlook.com>
2023-05-25 15:03:17 +0800

    add static and templates

---
+++ static/Analytics_baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a12cd184c2c3fbcdfded3b344eaf0f2b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
