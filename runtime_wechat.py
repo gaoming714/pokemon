c576e3cdb0841d91ee4faabb63c3589c62e76245
gaoming714 <gaoming714@outlook.com>
2023-10-14 11:21:35 +0800

    add callback to wechat

--- runtime_wechat.py
+++ runtime_wechat.py
@@ -4,39 +4,65 @@ import json
 import pendulum
 from flask import Flask, request, jsonify, Response
 import itchat
+from envelopes import Envelope, GMailSMTP
+from util import color
 
 from loguru import logger
 logger.add("log/wechat.log")
 
-## wechat profile
-chat_path = os.path.join("data", "chat_config.json")
-with open(chat_path, 'r', encoding='utf-8') as f:
-    chat_list = json.load(f)
+EMAIL = {}
+ME = ""
 
-user_list = chat_list["user_list"]
-chatroom_list = chat_list["chatroom_list"]
+USERS = []
+CHATROOMS = []
+
+## wechat profile
+# chat_path = os.path.join("data", "chat_config.json")
+# with open(chat_path, 'r', encoding='utf-8') as f:
+#     chat_dict = json.load(f)
+
+# USERS = chat_list["user_list"]
+# CHATROOMS = chat_list["chatroom_list"]
 
 app = Flask(__name__)
 
-def login_wechat_auto():
-    # itchat.auto_login(enableCmdQR=2)
-    # itchat.auto_login(hotReload=True)
-    itchat.auto_login()
-    payload = ""
-    str_list = ["🍒 => 买  Buy ", "\n",
-                "🍏 => 卖  Sell", "\n",
-                "🍌 => 量  amount", "\n",
-                "🔵 => 开  open", "\n",
-                "🔷 => 平  close", "\n",
-                "🍍 => 阈  area", "\n",
-                (pendulum.now("Asia/Shanghai")).strftime("%Y-%m-%d %H:%M:%S")
-                ]
-    payload = payload.join(str_list)
-    itchat.send(payload, toUserName='filehelper')
+def get_mixin():
+    global EMAIL
+    global ME
+    global USERS
+    global CHATROOMS
 
-# @app.route('/login',methods=['GET','POST'])
+    info_path = os.path.join("data", "chat_config.json")
+    try:
+        with open(info_path, 'r', encoding='utf-8') as file:
+            info_dict = json.load(file)
+        EMAIL = info_dict['email']
+        ME = info_dict['addr_list'][0]
+        USERS = info_dict["user_list"]
+        CHATROOMS = info_dict["chatroom_list"]
+    except:
+        logger.warning("chat_config.json is not ready")
+        raise
+
+# def login_wechat_auto():
+#     # itchat.auto_login(enableCmdQR=2)
+#     # itchat.auto_login(hotReload=True)
+#     itchat.auto_login()
+#     payload = ""
+#     str_list = ["🍒 => 买  Buy ", "\n",
+#                 "🍏 => 卖  Sell", "\n",
+#                 "🍌 => 量  amount", "\n",
+#                 "🔵 => 开  open", "\n",
+#                 "🔷 => 平  close", "\n",
+#                 "🍍 => 阈  area", "\n",
+#                 (pendulum.now("Asia/Shanghai")).strftime("%Y-%m-%d %H:%M:%S")
+#                 ]
+#     payload = payload.join(str_list)
+#     itchat.send(payload, toUserName='filehelper')
+
+
 def login_wechat():
-    itchat.auto_login(enableCmdQR=2)
+    itchat.auto_login(enableCmdQR=2, exitCallback=callbackEC)
     payload = ""
     str_list = ["🍓 🍏 🍌 ", "\n",
                 "Login in Successful.", "\n",
@@ -46,25 +72,38 @@ def login_wechat():
     itchat.send(payload, toUserName='filehelper')
     return 0
 
-# @app.route('/login',methods=['GET','POST'])
-# def test_wechat():
-#     with open(Path("QR.png"), 'rb') as f:
-#         image = f.read()
-#     return Response(image, mimetype='image/jpeg')
-
-
 @app.route('/msg/<msg>',methods=['GET','POST'])
 def send_message(msg):
     itchat.send(msg, toUserName='filehelper')
-    for user in user_list:
+    for user in USERS:
         name = itchat.search_friends(name=user)
         itchat.send(msg, toUserName=name[0]["UserName"])
-    for user in chatroom_list:
+    for user in CHATROOMS:
         name = itchat.search_chatrooms(name=user)
         itchat.send(msg, toUserName=name[0]["UserName"])
     # return jsonify(args=args, form=form)
     return json.dumps(msg)
 
+def callbackEC():
+    now = pendulum.now("Asia/Shanghai")
+    now_str = now.to_datetime_string()
+    email(ME, now_str)
+    color('Logout wechat')
+
+def email(addr,msg):
+    global EMAIL
+    envelope = Envelope(
+        from_addr = (EMAIL['from'], 'PokeScript'),
+        to_addr = (addr, 'Hi Jack'),
+        subject = 'Logout wechat',
+        text_body = msg
+    )
+
+    # Send the envelope using an ad-hoc connection...
+    envelope.send(EMAIL['smtp'], port=EMAIL['port'], login=EMAIL['login'],
+                password=EMAIL['password'], tls=True)
+
 if __name__ == '__main__':
+    get_mixin()
     login_wechat()
     app.run(port=8010)
