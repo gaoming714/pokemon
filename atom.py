096897963f1ad4522aa91210fa72199527f0466e
gaoming714 <gaoming714@outlook.com>
2024-04-10 20:34:39 +0800

    add upload

--- atom.py
+++ atom.py
@@ -398,5 +398,61 @@ def logout():
     flask_login.logout_user()
     return "Logged out"
 
+
+
+#upload
+from flask import flash
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+from werkzeug.utils import secure_filename
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'GET':
+            return render_template('upload.html')
+        # return '''
+        #             <!doctype html>
+        #             <title>Upload new File</title>
+        #             <h1>Upload new File</h1>
+        #             <form method=post enctype=multipart/form-data>
+        #             <input type=file name=file>
+        #             <input type=submit value=Upload>
+        #             </form>
+        #             '''
+
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            now = pendulum.now("Asia/Shanghai")
+            filename = str(now.timestamp()) + "__" + secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "<p>OK</p>"
+            # return redirect(url_for('show_file', name=filename))
+
+from flask import send_from_directory
+
+@app.route('/show_file/<name>')
+def show_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+
 if __name__ == '__main__':
     app.run(debug=True,port=8009)
